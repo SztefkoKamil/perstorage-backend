75d8491f130219288e7bc2f3649dc225660171a8
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-14 09:55:12 +0100

    add body-parser & CORS headers
    Added body-parser module.
    Added CORS headers for Origin, Methods & Headers.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 app.listen(8000);

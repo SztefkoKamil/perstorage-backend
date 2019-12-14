63401950492ffa3655fb1dbe7915bdc20494b59b
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-14 10:06:49 +0100

    Add empty routes
    Added empty middlewares for auth & storage routes.

---
+++ routes/storage.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/files', (req, res, next) => {
+
+});
+
+router.post('/file', (req, res, next) => {
+
+});
+
+router.put('/file', (req, res, next) => {
+
+});
+
+router.delete('/file', (req, res, next) => {
+
+});
+
+
+module.exports = router;

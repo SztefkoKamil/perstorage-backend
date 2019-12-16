d5da20bf08a98b61b72f6b08ea7a43f4fc778b24
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-16 14:01:44 +0100

    Add ToDo comments to controller files

--- controller/auth.js
+++ controller/auth.js
@@ -1,9 +1,39 @@
-
+const User = require('../model/user');
 
 exports.signup = (req, res, next) => {
+  // data validation here
 
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+
+  // check if user exist
+
+  // password encryption here
+
+  // create new User here
+
+  // send response
 };
 
 exports.login = (req, res, next) => {
+  // data validation here
   
+  // check if user exist
+
+  // decrypt and check password
+
+  // create jwt token
+
+  // send response with token
+};
+
+exports.delete = (req, res, next) => {
+  // data validation here
+  
+  // check if user exist
+
+  // delete user
+
+  // send response for redirect
 };
--- controller/storage.js
+++ controller/storage.js
@@ -1,17 +1,43 @@
 
 
 exports.getFiles = (req, res, next) => {
-  
+  // check authorization
+
+  // fetch files from DB
+
+  // send files
 };
 
 exports.uploadFile = (req, res, next) => {
+  // check authorization
   
+  // add file to DB
+
+  // send response
 };
 
 exports.updateFile = (req, res, next) => {
+  // check authorization
+
+  const fileId = req.params.id;
+
+  // find file in user's storage collection
   
+  // compare fields to update
+
+  // update file
+
+  //send response
 };
 
 exports.deleteFile = (req, res, next) => {
-  
+  // check authorization
+
+  const fileId = req.params.id;
+
+  // find file in user's storage collection
+
+  // delete file
+
+  // send response
 };

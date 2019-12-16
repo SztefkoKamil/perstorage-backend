8642d3fbc83ee4abb30b7a9c09f52b98e54a2af2
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-16 16:07:31 +0100

    Add validation to routes/storage.js

--- routes/storage.js
+++ routes/storage.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { body, param } = require('express-validator');
 const storageController = require('../controller/storage');
 
 router.get('/files', storageController.getFiles);
 
 router.post('/file', storageController.uploadFile);
 
-router.put('/file', storageController.updateFile);
+router.put('/file/:id', [
+  param('id').isMongoId()
+], storageController.updateFile);
 
-router.delete('/file', storageController.deleteFile);
+router.delete('/file/:id', [
+  param('id').isMongoId()
+], storageController.deleteFile);
 
 
 module.exports = router;

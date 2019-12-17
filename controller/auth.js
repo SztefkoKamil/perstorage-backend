f067116fd06fc0ad932397991b048eafc882a84d
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-17 18:22:22 +0100

    Correct signup middleware

--- controller/auth.js
+++ controller/auth.js
@@ -1,9 +1,9 @@
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const User = require('../model/user');
 
 exports.signup = async (req, res, next) => {
-  // data validation here
+  
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     console.log(errors.array());
@@ -11,25 +11,29 @@ exports.signup = async (req, res, next) => {
     error.statusCode = 422;
     error.data = errors.array();
     throw error;
- }
-
+  }
+  
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
 
   try {
-  // password encryption here
   const hashedPass = await bcrypt.hash(password, 12);
 
-  // create new User here
   const newUser = new User({
     email,
     name,
     password: hashedPass
   });
+  const savedUser = await newUser.save();
 
-  // send response
-  res.json(newUser);
+  const response = {
+    id: savedUser._id,
+    email: savedUser.email,
+    name: savedUser.name
+  };
+  res.json(response);
+
   } catch (err) {
     console.log(err);
     if (!err.statusCode) {

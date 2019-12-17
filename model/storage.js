998d5561016e93eac8555e09c660bb2ef276cbae
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-17 17:06:53 +0100

    Rebuild storage schema

--- model/storage.js
+++ model/storage.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const storageSchema = new Schema({
+const fileSchema = new Schema({
   owner: { type: Schema.Types.ObjectId, ref: 'User' },
-  files: [{
-    name: { type: String, required: true},
-    path: { type: String, required: true},
-    type: { type: String, required: true},
-    createdAt: { type: String, required: true},
-    updatedAt: { type: String }
-  }]
-});
+  name: { type: String, required: true},
+  ext: { type: String, required: true},
+  path: { type: String, required: true},
+  type: { type: String, required: true},
+}, { timestamps: true });
 
 
-module.exports = mongoose.model('Storage', storageSchema);
+module.exports = mongoose.model('File', fileSchema);

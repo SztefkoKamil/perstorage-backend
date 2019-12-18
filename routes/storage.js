34d8238d4c555af0ef6c139869d7d6690cfa3692
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-18 10:06:07 +0100

    Add multerParse to uploadFile route

--- routes/storage.js
+++ routes/storage.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const { body, param } = require('express-validator');
 const storageController = require('../controller/storage');
 const isAuth = require('../utils/isAuth');
+const multerParse = require('../utils/multerParse');
 
 router.get('/files', isAuth, storageController.getFiles);
 
-router.post('/file', isAuth, storageController.uploadFile);
+router.post('/file', isAuth, multerParse, storageController.uploadFile);
 
 router.put('/file/:id', isAuth, [
   param('id').isMongoId()

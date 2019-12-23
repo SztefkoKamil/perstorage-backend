6df0ae4d89a3f04c67f8442debdce61f059a7372
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-23 09:04:37 +0100

    Add errorCodes

--- utils/isAuth.js
+++ utils/isAuth.js
@@ -4,8 +4,9 @@ module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
 
   if (!authHeader) {
-    const error = new Error('Not authenticated');
+    const error = new Error('Authorization filed - no token');
     error.statusCode = 401;
+    error.errorCode = 900;
     throw error;
   }
 
@@ -15,13 +16,15 @@ module.exports = (req, res, next) => {
   try {
     decodedToken = jwt.verify(token, process.env.JWT_LOGIN_SECRET);
   } catch (err) {
-    err.statusCode = 500;
+    err.statusCode = 401;
+    err.errorCode = 901;
     throw err;
   }
 
   if (!decodedToken) {
-    const error = new Error('Not authenticated');
+    const error = new Error('Authorization filed - bad token');
     error.statusCode = 401;
+    error.errorCode = 901;
     throw error;
   }
 

14fb5e5c2658faf463a01dd1e52190e4607e881b
Kamil Sztefko <sztefkokamil@gmail.com>
2019-12-23 09:05:48 +0100

    Add errorCode to response

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,7 +1,8 @@
 module.exports = (error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
+  const errorCode = error.errorCode
   const message = error.message;
   const data = error.data;
-  res.status(status).json({message, data});
+  res.status(status).json({errorCode, message, data});
 }
